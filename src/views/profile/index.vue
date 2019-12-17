eb78ff4f3f99b10f91faa8efa26d4500951f6903
rjw <15666966389@163.com>
2019-12-17 18:55:38 +0800

    bug fixes

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -2,7 +2,6 @@
   <div class="app-container">
     <div v-if="user">
       <el-row :gutter="20">
-
         <el-col :span="6" :xs="24">
           <user-card :user="user" />
         </el-col>
@@ -22,7 +21,6 @@
             </el-tabs>
           </el-card>
         </el-col>
-
       </el-row>
     </div>
   </div>
@@ -45,12 +43,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters([
-      'name',
-      'avatar',
-      'roles',
-      'introduction'
-    ])
+    ...mapGetters(['name', 'avatar', 'roles', 'email', 'introduction'])
   },
   created() {
     this.getUser()
@@ -60,7 +53,7 @@ export default {
       this.user = {
         name: this.name,
         role: this.roles.join(' | '),
-        email: 'admin@test.com',
+        email: this.email,
         avatar: this.avatar,
         introduction: this.introduction
       }
