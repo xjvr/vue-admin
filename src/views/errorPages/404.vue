14c1cda472af6cd2cad517b1c0b9ea89308bdddc
rjw <i@jianwei.ren>
2019-12-18 22:45:43 +0800

    beautify 404 page

--- src/views/errorPages/404.vue
+++ src/views/errorPages/404.vue
@@ -1,22 +1,21 @@
 <template>
-  <div class="wscn-http404-container">
-    <div class="wscn-http404">
-      <div class="pic-404">
+  <div class="errPage-container">
+    <el-row :gutter="20">
+      <el-col :md="14" :sm="16" :xs="24" class="pic-404">
         <img class="pic-404__parent" src="@/assets/404_images/404.png" alt="404">
         <img class="pic-404__child left" src="@/assets/404_images/404_cloud.png" alt="404">
         <img class="pic-404__child mid" src="@/assets/404_images/404_cloud.png" alt="404">
         <img class="pic-404__child right" src="@/assets/404_images/404_cloud.png" alt="404">
-      </div>
-      <div class="bullshit">
-        <div class="bullshit__oops">OOPS!</div>
-        <div class="bullshit__info">All rights reserved
-          <a style="color:#20a0ff" href="https://wallstreetcn.com" target="_blank">wallstreetcn</a>
+      </el-col>
+      <el-col :md="10" :sm="8" :xs="24">
+        <div class="bullshit">
+          <div class="bullshit__oops">OOPS!</div>
+          <div class="bullshit__headline">{{ message }}</div>
+          <div class="bullshit__info">请检查你输入的URL是否正确，或者点击下面的按钮回到首页。</div>
+          <a href="" class="bullshit__return-home">返回首页</a>
         </div>
-        <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">Please check that the URL you entered is correct, or click the button below to return to the homepage.</div>
-        <a href="" class="bullshit__return-home">返回首页</a>
-      </div>
-    </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -26,28 +25,22 @@ export default {
   name: 'Page404',
   computed: {
     message() {
-      return 'The webmaster said that you can not enter this page...'
+      // return 'The webmaster said that you can not enter this page...'
+      return '啊哦，你访问的页面不存在(⋟﹏⋞)'
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.wscn-http404-container{
-  transform: translate(-50%,-50%);
-  position: absolute;
-  top: 40%;
-  left: 50%;
-}
-.wscn-http404 {
-  position: relative;
-  width: 1200px;
-  padding: 0 50px;
-  overflow: hidden;
+.errPage-container {
+  max-width: 1000px;
+  margin: 10% auto 0;
+  padding: 0 5%;
+  text-align: center;
   .pic-404 {
     position: relative;
     float: left;
-    width: 600px;
     overflow: hidden;
     &__parent {
       width: 100%;
@@ -55,9 +48,9 @@ export default {
     &__child {
       position: absolute;
       &.left {
-        width: 80px;
-        top: 17px;
-        left: 220px;
+        width: 15%;
+        top: 12%;
+        left: 32%;
         opacity: 0;
         animation-name: cloudLeft;
         animation-duration: 2s;
@@ -66,9 +59,9 @@ export default {
         animation-delay: 1s;
       }
       &.mid {
-        width: 46px;
-        top: 10px;
-        left: 420px;
+        width: 8%;
+        top: 5%;
+        left: 55%;
         opacity: 0;
         animation-name: cloudMid;
         animation-duration: 2s;
@@ -77,9 +70,9 @@ export default {
         animation-delay: 1.2s;
       }
       &.right {
-        width: 62px;
-        top: 100px;
-        left: 500px;
+        width: 10%;
+        top: 30%;
+        left: 70%;
         opacity: 0;
         animation-name: cloudRight;
         animation-duration: 2s;
@@ -155,19 +148,21 @@ export default {
       }
     }
   }
+
   .bullshit {
     position: relative;
     float: left;
-    width: 300px;
+    max-width: 300px;
     padding: 30px 0;
     overflow: hidden;
+    text-align: left;
     &__oops {
-      font-size: 32px;
+      font-size: 40px;
       font-weight: bold;
-      line-height: 40px;
+      line-height: 48px;
       color: #1482f0;
       opacity: 0;
-      margin-bottom: 20px;
+      margin-bottom: 30px;
       animation-name: slideUp;
       animation-duration: 0.5s;
       animation-fill-mode: forwards;
@@ -178,7 +173,7 @@ export default {
       color: #222;
       font-weight: bold;
       opacity: 0;
-      margin-bottom: 10px;
+      margin-bottom: 20px;
       animation-name: slideUp;
       animation-duration: 0.5s;
       animation-delay: 0.1s;
@@ -224,5 +219,6 @@ export default {
       }
     }
   }
+
 }
 </style>
